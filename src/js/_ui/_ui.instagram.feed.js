30f2ca4a9bc5803ea2e62a9db39c8ea1ef8502ca
Tony Air <tony@thewebdevelopmentagency.com>
2021-02-26 09:47:36 +0700

    FIX: minor fixtures

--- src/js/_ui/_ui.instagram.feed.js
+++ src/js/_ui/_ui.instagram.feed.js
@@ -12,10 +12,30 @@ export default ((W) => {
   const D = document;
   const BODY = D.body;
 
+  const ig_media_preview = (base64data) => {
+    const jpegtpl =
+            '/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEABsaGikdKUEmJkFCLy8vQkc/Pj4/R0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0cBHSkpNCY0PygoP0c/NT9HR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR0dHR//AABEIABQAKgMBIgACEQEDEQH/xAGiAAABBQEBAQEBAQAAAAAAAAAAAQIDBAUGBwgJCgsQAAIBAwMCBAMFBQQEAAABfQECAwAEEQUSITFBBhNRYQcicRQygZGhCCNCscEVUtHwJDNicoIJChYXGBkaJSYnKCkqNDU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6g4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2drh4uPk5ebn6Onq8fLz9PX29/j5+gEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoLEQACAQIEBAMEBwUEBAABAncAAQIDEQQFITEGEkFRB2FxEyIygQgUQpGhscEJIzNS8BVictEKFiQ04SXxFxgZGiYnKCkqNTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqCg4SFhoeIiYqSk5SVlpeYmZqio6Slpqeoqaqys7S1tre4ubrCw8TFxsfIycrS09TV1tfY2dri4+Tl5ufo6ery8/T19vf4+fr/2gAMAwEAAhEDEQA/AA==',
+      t = atob(base64data),
+      p = t.slice(3).split(''),
+      o = t
+        .substring(0, 3)
+        .split('')
+        .map((e) => {
+          return e.charCodeAt(0);
+        }),
+      c = atob(jpegtpl).split('');
+    c[162] = String.fromCharCode(o[1]);
+    c[160] = String.fromCharCode(o[2]);
+    return base64data ?
+      `data:image/jpeg;base64,${btoa(c.concat(p).join(''))}` :
+      null;
+  };
+
   const loadFeed = () => {
     console.log(`${NAME}: loading`);
 
-    D.querySelectorAll(`.jsInstagramFeed`).forEach((el) => {
+    D.querySelectorAll(`.jsInstagramFeed`).forEach((el, i) => {
+      const ID = `InstagramFeed${i}`;
       const dataset = el.dataset;
 
       el.classList.add(`${NAME}-loading`);
@@ -42,13 +62,11 @@ export default ((W) => {
           data['edge_owner_to_timeline_media']['edges'].forEach(
             (el, i) => {
               const item = el['node'];
-              const preview = ui.ig_media_preview(
-                item['media_preview'],
-              );
+              const preview = ig_media_preview(item['media_preview']);
 
               list.innerHTML +=
                                 `<div class="a col ${NAME}-item"` +
-                                ` data-lightbox-gallery="${NAME}-${ID}" data-href="${item['display_url']}" data-force="image">` +
+                                ` data-gallery="${NAME}-${ID}" data-href="${item['display_url']}" data-toggle="lightbox" data-force="image">` +
                                 `<img id="${NAME}-${ID}-${item['id']}" src="${item['display_url']}" alt="${item['accessibility_caption']}"` +
                                 `style="background:url(${preview})" />` +
                                 '</div>';
