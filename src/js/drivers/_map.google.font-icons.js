a422116f1713d4d3313fbc86c491b1858991fd21
Tony Air <tony@twma.pro>
2021-07-20 00:36:53 +0200

    Rewrite Maps API to vanilla js

---
+++ src/js/drivers/_map.google.font-icons.js
@@ -0,0 +1,199 @@
+'use strict';
+
+const Obj = {
+  init: () => {
+    class GoogleMapsHtmlOverlay extends google.maps.OverlayView {
+      constructor(options) {
+        super();
+        const ui = this;
+
+        ui.setMap(options.map);
+        ui.position = options.position;
+        ui.html =
+          (options.html ?
+            options.html :
+            '<div class="mapboxgl-marker"><i class="marker-icon fas fa-map-marker-alt"></i></div>'
+          );
+        ui.divClass = options.divClass;
+        ui.align = options.align;
+        ui.isDebugMode = options.debug;
+        ui.onClick = options.onClick;
+        ui.onMouseOver = options.onMouseOver;
+
+        ui.isBoolean = (arg) => {
+          if (typeof arg === 'boolean') {
+            return true;
+          } else {
+            return false;
+          }
+        };
+
+        ui.isNotUndefined = (arg) => {
+          if (typeof arg !== 'undefined') {
+            return true;
+          } else {
+            return false;
+          }
+        };
+
+        ui.hasContent = (arg) => {
+          if (arg.length > 0) {
+            return true;
+          } else {
+            return false;
+          }
+        };
+
+        ui.isString = (arg) => {
+          if (typeof arg === 'string') {
+            return true;
+          } else {
+            return false;
+          }
+        };
+
+        ui.isFunction = (arg) => {
+          if (typeof arg === 'function') {
+            return true;
+          } else {
+            return false;
+          }
+        };
+      }
+      onAdd() {
+        const ui = this;
+
+        // Create div element.
+        ui.div = document.createElement('div');
+        ui.div.style.position = 'absolute';
+
+        // Validate and set custom div class
+        if (ui.isNotUndefined(ui.divClass) && ui.hasContent(ui.divClass))
+          ui.div.className = ui.divClass;
+
+        // Validate and set custom HTML
+        if (
+          ui.isNotUndefined(ui.html) &&
+          ui.hasContent(ui.html) &&
+          ui.isString(ui.html)
+        )
+          ui.div.innerHTML = ui.html;
+
+        // If debug mode is enabled custom content will be replaced with debug content
+        if (ui.isBoolean(ui.isDebugMode) && ui.isDebugMode) {
+          ui.div.className = 'debug-mode';
+          ui.div.innerHTML =
+            '<div style="height: 10px; width: 10px; background: red; border-radius: 100%;"></div>' +
+            '<div style="position: absolute; top: 5px; padding: 5px; width: 130px; text-align: center; font-size: 18px; text-transform: uppercase; font-weight: bolder; background: red; color: white; font-family: Arial;">Debug mode</div>';
+          ui.div.setAttribute(
+            'style',
+            'position: absolute;' +
+            'border: 5px dashed red;' +
+            'height: 150px;' +
+            'width: 150px;' +
+            'display: flex;' +
+            'justify-content: center;' +
+            'align-items: center;'
+          );
+        }
+
+        // Add element to clickable layer
+        ui.getPanes().overlayMouseTarget.appendChild(ui.div);
+
+        // Add listeners to the element.
+        google.maps.event.addDomListener(ui.div, 'click', (event) => {
+          google.maps.event.trigger(ui, 'click');
+          if (ui.isFunction(ui.onClick)) ui.onClick();
+          event.stopPropagation();
+        });
+
+        google.maps.event.addDomListener(ui.div, 'mouseover', (event) => {
+          google.maps.event.trigger(ui, 'mouseover');
+          if (ui.isFunction(ui.onMouseOver)) ui.onMouseOver();
+          event.stopPropagation();
+        });
+      }
+
+      draw() {
+        const ui = this;
+
+        // Calculate position of div
+        var positionInPixels = ui.getProjection().fromLatLngToDivPixel(
+          new google.maps.LatLng(ui.position)
+        );
+
+        // Align HTML overlay relative to original position
+        var divOffset = {
+          y: undefined,
+          x: undefined,
+        };
+
+        switch (Array.isArray(ui.align) ? ui.align.join(' ') : '') {
+          case 'left top':
+            divOffset.y = ui.div.offsetHeight;
+            divOffset.x = ui.div.offsetWidth;
+            break;
+          case 'left center':
+            divOffset.y = ui.div.offsetHeight / 2;
+            divOffset.x = ui.div.offsetWidth;
+            break;
+          case 'left bottom':
+            divOffset.y = 0;
+            divOffset.x = ui.div.offsetWidth;
+            break;
+          case 'center top':
+            divOffset.y = ui.div.offsetHeight;
+            divOffset.x = ui.div.offsetWidth / 2;
+            break;
+          case 'center center':
+            divOffset.y = ui.div.offsetHeight / 2;
+            divOffset.x = ui.div.offsetWidth / 2;
+            break;
+          case 'center bottom':
+            divOffset.y = 0;
+            divOffset.x = ui.div.offsetWidth / 2;
+            break;
+          case 'right top':
+            divOffset.y = ui.div.offsetHeight;
+            divOffset.x = 0;
+            break;
+          case 'right center':
+            divOffset.y = ui.div.offsetHeight / 2;
+            divOffset.x = 0;
+            break;
+          case 'right bottom':
+            divOffset.y = 0;
+            divOffset.x = 0;
+            break;
+          default:
+            divOffset.y = ui.div.offsetHeight / 2;
+            divOffset.x = ui.div.offsetWidth / 2;
+        }
+
+        // Set position
+        ui.div.style.top = `${positionInPixels.y - divOffset.y  }px`;
+        ui.div.style.left = `${positionInPixels.x - divOffset.x  }px`;
+      }
+
+      getPosition() {
+        const ui = this;
+        return ui.position;
+      }
+
+      getDiv() {
+        const ui = this;
+        return ui.div;
+      }
+
+      setPosition(position, align) {
+        const ui = this;
+        ui.position = position;
+        ui.align = align;
+        ui.draw();
+      }
+    }
+    return GoogleMapsHtmlOverlay;
+  },
+}
+
+export default Obj;
---
+++ src/js/drivers/_map.mapbox.js
@@ -0,0 +1,187 @@
+'use strict';
+
+import $ from 'jquery';
+import mapBoxGL from 'mapbox-gl';
+
+import Events from '../../_events';
+
+const MapBoxDriver = (($) => {
+  class MapBoxDriver {
+    getName() {
+      return 'MapBoxDriver';
+    }
+
+    init($el, config = []) {
+      const ui = this;
+
+      mapBoxGL.accessToken = config['key'];
+
+      ui.map = new mapBoxGL.Map({
+        container: $el.find('.mapAPI-map')[0],
+        center: config['center'] ? config['center'] : [0, 0],
+        //hash: true,
+        style: config['style']
+          ? config['style']
+          : 'mapbox://styles/mapbox/streets-v9',
+        localIdeographFontFamily: config['font-family'],
+        zoom: config['mapZoom'] ? config['mapZoom'] : 10,
+        attributionControl: false,
+        antialias: true,
+        accessToken: config['key'],
+      })
+        .addControl(
+          new mapBoxGL.AttributionControl({
+            compact: true,
+          }),
+        )
+        .addControl(new mapBoxGL.NavigationControl(), 'top-right')
+        .addControl(
+          new mapBoxGL.GeolocateControl({
+            positionOptions: {
+              enableHighAccuracy: true,
+            },
+            trackUserLocation: true,
+          }),
+          'bottom-right',
+        )
+        .addControl(
+          new mapBoxGL.ScaleControl({
+            maxWidth: 80,
+            unit: 'metric',
+          }),
+          'top-left',
+        )
+        .addControl(new mapBoxGL.FullscreenControl());
+
+      ui.map.on('load', (e) => {
+        $el.trigger(Events.MAPAPILOADED);
+      });
+
+      ui.popup = new mapBoxGL.Popup({
+        closeOnClick: false,
+        className: 'popup',
+      });
+    }
+
+    addMarker(crds, config) {
+      const ui = this;
+
+      // create a DOM el for the marker
+      const $el = $(
+        `<div id="Marker${config['id']}" data-id="${config['id']}" class="marker">${config['icon']}</div>`,
+      );
+
+      $el.on('click', (e) => {
+        ui.popup
+          .setLngLat(crds)
+          .setHTML(config['content'])
+          .addTo(ui.map);
+
+        if (config['flyToMarker']) {
+          ui.map.flyTo({
+            center: crds,
+            zoom: 17,
+          });
+        }
+
+        $(e.currentTarget).trigger(Events.MAPMARKERCLICK);
+      });
+
+      // add marker to map
+      const marker = new mapBoxGL.Marker($el[0]).setLngLat(crds).addTo(ui.map);
+
+      return marker;
+    }
+
+    addGeoJson(config) {
+      const ui = this;
+      // Insert the layer beneath any symbol layer.
+      /*if (config['3d']) {
+        const layers = Map.getStyle().layers;
+        let labelLayerId;
+        for (let i = 0; i < layers.length; i++) {
+          if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+            labelLayerId = layers[i].id;
+            break;
+          }
+        }
+
+        Map.addLayer({
+          'id': '3d-buildings',
+          'source': 'composite',
+          'source-layer': 'building',
+          'filter': ['==', 'extrude', 'true'],
+          'type': 'fill-extrusion',flyToBounds
+          'minzoom': 15,
+          'paint': {
+            'fill-extrusion-color': '#aaa',
+
+            // use an 'interpolate' expression to add a smooth transition effect to the
+            // buildings as the user zooms in
+            'fill-extrusion-height': [
+              "interpolate", ["linear"],
+              ["zoom"],
+              15, 0,
+              15.05, ["get", "height"],
+            ],
+            'fill-extrusion-base': [
+              "interpolate", ["linear"],
+              ["zoom"],
+              15, 0,
+              15.05, ["get", "min_height"],
+            ],
+            'fill-extrusion-opacity': .6,
+          },
+        }, labelLayerId);
+      }*/
+
+      const firstMarker = config['geojson'].features[0].geometry.coordinates;
+      //Map.setCenter(firstMarker);
+      const bounds = new mapBoxGL.LngLatBounds(firstMarker, firstMarker);
+
+      // add markers to map
+      config['geojson'].features.forEach((marker) => {
+        const id = marker.id;
+        const crds = marker.geometry.coordinates;
+        const content = marker.properties.content;
+
+        ui.addMarker(crds, {
+          id,
+          content,
+          icon: marker.icon,
+          flyToMarker: config['flyToMarker'],
+        });
+
+        bounds.extend(crds);
+      });
+
+      ui.map.fitBounds(bounds, {
+        padding: 30,
+      });
+
+      ui.popup.on('close', (e) => {
+        if (config['flyToBounds']) {
+          ui.map.fitBounds(bounds, {
+            padding: 30,
+          });
+        }
+
+        $(e.currentTarget).trigger(Events.MAPPOPUPCLOSE);
+      });
+    }
+
+    getMap() {
+      const ui = this;
+      return ui.map;
+    }
+
+    getPopup() {
+      const ui = this;
+      return ui.popup;
+    }
+  }
+
+  return MapBoxDriver;
+})($);
+
+export default MapBoxDriver;
